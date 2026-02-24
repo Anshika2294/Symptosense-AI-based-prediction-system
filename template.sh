bda8aff1d18131285aae51455d5a235941c796fd
Anshika Shukla <anshika.shukla2294@gmail.com>
2026-02-24 20:59:32 +0530

    Added Medical Chatbot project

---
+++ template.sh
@@ -0,0 +1,17 @@
+#Creating directories for the project
+mkdir -p src
+mkdir -p reserach
+
+#Creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reseacrch/trails.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directory and files created successfully!"
+ 
